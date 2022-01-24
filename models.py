ad19c173ac61e50aa580c2dfd32e6f9c3b74e941
Oliver Jiang <oj@MyPC.local>
2022-01-24 09:04:26 -0500

    initial commit

---
+++ models.py
@@ -0,0 +1,44 @@
+# Animals
+# ID / Name / Habitat
+
+# Zookeeper log
+# ID / Animal ID (Foreign Key) / Notes
+
+from venv import create
+from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, relationship
+
+
+engine = create_engine("sqlite:///zoo.db", echo=False)
+Session = sessionmaker(bind=engine)
+session = Session()
+Base = declarative_base()
+
+class Animal(Base):
+    __tablename__ = "animals"
+    
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    habitat = Column(String)
+    logs = relationship("Logbook", back_populates="animal")
+
+    def __repr__(self):
+        return f"""
+            \nAnimal {self.id}
+            \rName = {self.name}
+            \rHabitat = {self.habitat}"""
+
+class Logbook(Base):
+    __tablename__= "logbook"  
+
+    id = Column(Integer, primary_key=True)
+    animal_id = Column(Integer, ForeignKey("animals.id"))
+    notes = Column(String)
+    animal = relationship("Animal", back_populates="logs")
+
+    def __repr__(self):
+        return f"""
+            \nLogbook {self.id}
+            \rAnimal ID = {self.animal_id}
+            \rNotes = {self.notes}"""
